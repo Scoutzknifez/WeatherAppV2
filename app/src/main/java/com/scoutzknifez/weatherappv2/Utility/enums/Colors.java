4bc6aab494ee6febdfd8809003e30de7ff3cdd37
Scoutzknifez <codygaming@4alexanders.com>
2019-12-23 23:38:52 -0800

    Added a color enum, linked with colors resource.

---
+++ app/src/main/java/com/scoutzknifez/weatherappv2/Utility/enums/Colors.java
@@ -0,0 +1,27 @@
+package com.scoutzknifez.weatherappv2.Utility.enums;
+
+import com.scoutzknifez.weatherappv2.R;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum Colors {
+    RED(R.color.red),
+    LIGHT_RED(R.color.light_red),
+    ORANGE(R.color.orange),
+    LIGHT_ORANGE(R.color.light_orange),
+    YELLOW(R.color.yellow),
+    // TODO Greens
+    LIGHT_BLUE(R.color.light_blue),
+    BLUE(R.color.blue),
+    DARK_BLUE(R.color.dark_blue),
+    // TODO Purples???
+
+    BLACK(R.color.black),
+    GRAY(R.color.gray),
+    WHITE(R.color.white);
+
+    int colorID;
+}
