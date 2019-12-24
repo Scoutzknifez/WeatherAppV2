875c6e11da46dab7c9ec5c80955c9f9b84915496
Scoutzknifez <codygaming@4alexanders.com>
2019-12-24 01:36:57 -0800

    Split the constants from globals respectively.

---
+++ app/src/main/java/com/scoutzknifez/weatherappv2/Utility/Constants.java
@@ -0,0 +1,13 @@
+package com.scoutzknifez.weatherappv2.Utility;
+
+public class Constants {
+    // Program wide variables
+    public static final String TAG = "WEATHER_APP_V2";
+
+    // Time
+    public static final int MILLIS_IN_SECOND = 1000;
+    public static final int MILLIS_IN_MINUTE = MILLIS_IN_SECOND * 60;
+    public static final int MILLIS_IN_HOUR = MILLIS_IN_MINUTE * 60;
+    public static final int MILLIS_IN_DAY = MILLIS_IN_HOUR * 24;
+    public static final int EPOCH_DAY = MILLIS_IN_DAY / 1000;
+}
---
+++ app/src/main/java/com/scoutzknifez/weatherappv2/Utility/Globals.java
@@ -0,0 +1,11 @@
+package com.scoutzknifez.weatherappv2.Utility;
+
+import androidx.fragment.app.FragmentTransaction;
+
+public class Globals {
+    // Program wide variables
+    public static boolean hasInternet = false;
+
+    // Android variables
+    public static FragmentTransaction fragmentTransaction;
+}
