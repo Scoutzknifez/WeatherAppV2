b81d89d3672e6ba450c4c5c77ef21c8bec21c26a
Scoutzknifez <codygaming@4alexanders.com>
2020-01-03 20:44:08 -0800

    Created generic queue structure.

---
+++ app/src/main/java/com/scoutzknifez/weatherappv2/structures/Queue.java
@@ -0,0 +1,27 @@
+package com.scoutzknifez.weatherappv2.structures;
+
+import java.util.ArrayList;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class Queue<T> {
+    private ArrayList<T> queue = new ArrayList<>();
+    private int maxSize = 10;
+
+    public Queue(int maxSize) {
+        setMaxSize(maxSize);
+    }
+
+    public T peek() {
+        return queue.size() > 0 ? queue.get(0) : null;
+    }
+
+    public void push(T newElement) {
+        queue.add(0, newElement);
+        if (queue.size() > maxSize)
+            queue.remove(maxSize);
+    }
+}
