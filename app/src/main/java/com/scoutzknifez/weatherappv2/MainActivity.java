8f5f2e19758ddd42df2184867e62de0c639bb428
Scoutzknifez <codygaming@4alexanders.com>
2020-01-04 22:55:31 -0800

    Fixed screen rotation recalling the API weather fetch.

--- app/src/main/java/com/scoutzknifez/weatherappv2/MainActivity.java
+++ app/src/main/java/com/scoutzknifez/weatherappv2/MainActivity.java
@@ -6,38 +6,27 @@ import android.os.Bundle;
 import android.view.WindowManager;
 
 import com.scoutzknifez.weatherappv2.Fragments.WeatherForecast;
+import com.scoutzknifez.weatherappv2.datafetcher.DataConnector;
+import com.scoutzknifez.weatherappv2.structures.Refresher;
 import com.scoutzknifez.weatherappv2.utility.Globals;
-import com.scoutzknifez.weatherappv2.utility.Utils;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         // Change Android Properties for app
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-        // Check if the fetch was successful
-        Thread thread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                Utils.initializeFetcher();
-            }
-        });
-        thread.start();
+        Globals.refresher = new Refresher();
+        while (Globals.recentWeatherData.peek() == null) {}
 
-        // find a way to hide the top and bottom bars on android OS default
-
-        try {
-            thread.join();
-        } catch (Exception e) {
-            System.out.println("Thread crashed");
-        }
+        Globals.mainForecastFragment = new WeatherForecast();
+        DataConnector.updatables.add(Globals.mainForecastFragment);
 
         Globals.fragmentTransaction = getSupportFragmentManager().beginTransaction();
-        Globals.fragmentTransaction.add(R.id.main_container, new WeatherForecast(), "fragment_weather_forecast");
+        Globals.fragmentTransaction.add(R.id.main_container, Globals.mainForecastFragment, "fragment_weather_forecast");
         Globals.fragmentTransaction.commit();
 
         setContentView(R.layout.activity_main);
