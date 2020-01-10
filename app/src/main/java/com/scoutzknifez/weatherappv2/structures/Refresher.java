ba3f5130001a59f43e88bb0b637aed2be9e21bcd
Scoutzknifez <codygaming@4alexanders.com>
2020-01-10 11:38:21 -0800

    Added a machine that operators on a separate thread to update the weather.

---
+++ app/src/main/java/com/scoutzknifez/weatherappv2/structures/Refresher.java
@@ -0,0 +1,60 @@
+package com.scoutzknifez.weatherappv2.structures;
+
+import androidx.core.util.Supplier;
+
+import com.scoutzknifez.weatherappv2.datafetcher.DataConnector;
+import com.scoutzknifez.weatherappv2.datafetcher.FetcherController;
+import com.scoutzknifez.weatherappv2.Fragments.interfaces.Updatable;
+import com.scoutzknifez.weatherappv2.structures.weather.WeatherDataPacket;
+import com.scoutzknifez.weatherappv2.utility.Constants;
+import com.scoutzknifez.weatherappv2.utility.Globals;
+import com.scoutzknifez.weatherappv2.utility.Utils;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class Refresher {
+    private Supplier<WeatherDataPacket> weatherRefresher = new Supplier<WeatherDataPacket>() {
+        @Override
+        public WeatherDataPacket get() {
+            return FetcherController.fetchWeather();
+        }
+    };
+
+    public Refresher() {
+        Thread thread = new Thread(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        queueRefresh();
+                    }
+                }
+        );
+
+        thread.start();
+    }
+
+    private void queueRefresh() {
+        try {
+            if (Utils.hasInternetConnection()) {
+                Globals.recentWeatherData.push(weatherRefresher.get());
+
+                try {
+                    for (Updatable updatable : DataConnector.updatables)
+                        updatable.update();
+                } catch (Exception e) {
+                    Utils.log("Could not update the screen because %s", e);
+                }
+
+                Thread.sleep(Constants.MILLIS_IN_MINUTE * 15);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            Utils.log("Could not wait for queue to refresh on %s", e);
+        }
+
+        queueRefresh();
+    }
+}
