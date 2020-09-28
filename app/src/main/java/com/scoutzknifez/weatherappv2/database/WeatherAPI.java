f2b4a6138438bb932a52894039e711fb71be12eb
Scoutzknifez <codygaming@4alexanders.com>
2020-09-27 23:37:08 -0700

    Added a call to be implemented into the API for fetching weather history

--- app/src/main/java/com/scoutzknifez/weatherappv2/database/WeatherAPI.java
+++ app/src/main/java/com/scoutzknifez/weatherappv2/database/WeatherAPI.java
@@ -5,14 +5,18 @@ import com.scoutzknifez.weatherappv2.structures.dto.WeatherForTime;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
 import retrofit2.http.Headers;
 import retrofit2.http.PUT;
 
 public interface WeatherAPI {
-    // TODO Built API for this
-
     @PUT("/api/submitweather")
     @Headers("Accept: application/json")
-    Call<APIResult> postWeatherUpdate(@Body WeatherForTime weatherForTime);
     // https://stackoverflow.com/questions/21398598/how-to-post-raw-whole-json-in-the-body-of-a-retrofit-request
+    Call<APIResult> postWeatherUpdate(@Body WeatherForTime weatherForTime);
+
+    // TODO
+    @GET("/api/")
+    @Headers("Accept: application/json")
+    Call<APIResult> getWeatherDay();
 }
